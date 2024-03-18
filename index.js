62e9c84304336f4a5d8127ea4a729e22ffb791ff
Yohan Jayarathna <yohan.jayarathna@opencastsoftware.com>
2024-03-18 15:29:19 +0000

    basic risk logic added

--- index.js
+++ index.js
@@ -5,8 +5,34 @@ const RiskScorecard = (globalConfig) => {
     };
 
     const execute = async (inputs, config) => {
-        console.log(inputs);
+        console.log("config");
         console.log(config);
+        console.log("inputs");
+        console.log(inputs);
+        const upperValue = config['upper-value'];
+        const lowerValue = config['lower-value'];
+
+        console.log("upper value: " + upperValue);
+        console.log("lower value: " + lowerValue);
+
+        var sci = inputs[0].sci
+
+        console.log(sci);
+        
+        var riskColor = '';
+        if(sci < lowerValue){
+            riskColor = 'green';
+        }
+        else if(sci < upperValue) {
+            riskColor = "amber";
+        }
+        else
+        {
+            riskColor = 'red';
+        }
+
+        console.log("risk color: " + riskColor);
+        return riskColor;
     }
 
     return {
