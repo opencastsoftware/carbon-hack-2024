12ae2ce6c242fc231d0b654d8c4e4ab45b211439
omotayotalabi <omo4adex@gmail.com>
2024-03-19 13:54:48 +0000

    error handling in index.js Co-authored-by: Paul Olabisi <pauloolabisi@gmail.com> Co-authored-by: Talabi Omotayo <omo4adex@gmail.com> Co-authored-by: Yohan Jayarthna <yohan.jayarathna@opencastsoftware.com>

--- index.js
+++ index.js
@@ -4,13 +4,27 @@ const RiskScorecard = (globalConfig) => {
         kind: 'execute',
     };
 
+    
+    const validateConfig = (config) => {
+        // Example validation: Check if upper-value and lower-value are numbers
+        if (typeof config['upper-value'] !== 'number' || typeof config['lower-value'] !== 'number') {
+            throw new Error('upper-value and lower-value must be numbers');
+        }
+        // check if lower value is greater than upper value
+        if ( config['lower-value'] >= config['upper-value']) {
+            throw new Error('lower-value can not be greater than or equal to upper value');
+        }
+
+        return config;
+    };
     const execute = async (inputs, config) => {
         console.log("config");
         console.log(config);
         console.log("inputs");
         console.log(inputs);
-        const upperValue = config['upper-value'];
-        const lowerValue = config['lower-value'];
+        var safeConfig = validateConfig(config);
+        const upperValue = safeConfig['upper-value'];
+        const lowerValue = safeConfig['lower-value'];
 
         console.log("upper value: " + upperValue);
         console.log("lower value: " + lowerValue);
@@ -32,7 +46,7 @@ const RiskScorecard = (globalConfig) => {
         }
 
         console.log("risk color: " + riskColor);
-        return {['risk-color']: riskColor};;
+        return {['risk-color']: riskColor};
     }
 
     return {
