d74303f079f4c85db76372f0b1a736d89b5c67db
Yohan Jayarathna <yohan.jayarathna@opencastsoftware.com>
2024-03-19 17:05:10 +0000

    Unit test fixed for risk-scorecard
    Co-authored-by: Paul Olabisi <pauloolabisi@gmail.com>
    Co-authored-by: Talabi Omotayo <omo4adex@gmail.com>
    Co-authored-by: Yohan Jayarthna <yohan.jayarathna@opencastsoftware.com>

--- tests/risk-scorecard/RiskScorecard.test.js
+++ tests/risk-scorecard/RiskScorecard.test.js
@@ -1,42 +1,40 @@
-const { validateConfig, execute } = require('./plugins/risk-scorecard/index');
+const { RiskScorecard } = require('../../plugins/risk-scorecard/index');
 
 describe('RiskScorecard', () => {
-    describe('validateConfig', () => {
-        test('should throw error if upper-value is not a number', () => {
-            const config = { 'upper-value': 'not a number', 'lower-value': 10 };
-            expect(() => validateConfig(config)).toThrow('upper-value and lower-value must be numbers');
-        });
-
-        test('should throw error if lower-value is not a number', () => {
-            const config = { 'upper-value': 20, 'lower-value': 'not a number' };
-            expect(() => validateConfig(config)).toThrow('upper-value and lower-value must be numbers');
-        });
-
-        test('should return config if all validations pass', () => {
-            const config = { 'upper-value': 20, 'lower-value': 10 };
-            expect(validateConfig(config)).toEqual(config);
-        });
-    });
+    const riskScorecard = RiskScorecard({});
 
     describe('execute', () => {
+
+        test('should throw error if config is invalid', async () => {
+            const inputs = [{ sci: 15 }];
+            const config = { 'upper-value': 'not a number', 'lower-value': 10 };
+            await expect(riskScorecard.execute(inputs, config)).rejects.toThrow('upper-value and lower-value must be numbers');
+        });
+
+        test('should throw error if config is invalid', async () => {
+            const inputs = [{ sci: 15 }];
+            const config = { 'upper-value': 5, 'lower-value': 10 };
+            await expect(riskScorecard.execute(inputs, config)).rejects.toThrow('lower-value can not be greater than or equal to upper value');
+        });
+
         test('should return green risk color if sci is less than lower value', async () => {
             const inputs = [{ sci: 5 }];
             const config = { 'upper-value': 20, 'lower-value': 10 };
-            const result = await execute(inputs, config);
+            const result = await riskScorecard.execute(inputs, config);
             expect(result['risk-color']).toBe('green');
         });
 
-        test('should return amber risk color if sci is less than upper value', async () => {
+        test('should return amber risk color if sci is greater than lower value and less than upper value', async () => {
             const inputs = [{ sci: 15 }];
             const config = { 'upper-value': 20, 'lower-value': 10 };
-            const result = await execute(inputs, config);
+            const result = await riskScorecard.execute(inputs, config);
             expect(result['risk-color']).toBe('amber');
         });
 
         test('should return red risk color if sci is greater than upper value', async () => {
             const inputs = [{ sci: 25 }];
             const config = { 'upper-value': 20, 'lower-value': 10 };
-            const result = await execute(inputs, config);
+            const result = await riskScorecard.execute(inputs, config);
             expect(result['risk-color']).toBe('red');
         });
     });
